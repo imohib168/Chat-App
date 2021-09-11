c69e2746b8d161a8e815fa030c8ea0ea9ea244fb
Mohib Ismail <imohib168@gmail.com>
2021-09-11 18:43:02 +0500

    Chat App made using sockets.io

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: { origin: "*" }
+});
+
+app.get('/', (req, res) => {
+    res.send('Hi, Server is running')
+})
+
+io.on('connection', (socket) => {
+    console.log('Socket is active to be connected');
+
+    socket.on("chat", (payload) => {
+        io.emit("chat", payload);
+    })
+})
+
+// app.listen(5000, () => console.log(`Server is listening at port 5000`))
+server.listen(5000, () => console.log(`Server is listening at port 5000`))
---
+++ frontend/chatty/src/App.js
@@ -0,0 +1,75 @@
+import './App.css'
+
+import React, { useState, useEffect, useRef } from 'react';
+import socket from 'socket.io-client';
+import { nanoid } from 'nanoid';
+
+// import { css } from 'emotion';
+// import ScrollToBottom from 'react-scroll-to-bottom';
+
+// const ROOT_CSS = {
+//   height: 600,
+//   width: 400
+// }
+
+// const socket = io.connect('http://localhost:5000');
+
+const userName = nanoid(4);
+
+function App() {
+  const [message, setMessage] = useState('');
+  const [chat, setChat] = useState([]);
+  const socketClientRef = useRef();
+
+  const sendChat = (e) => {
+    e.preventDefault();
+    socketClientRef.current.emit("chat", { message, userName })
+    setMessage('');
+  }
+
+  useEffect(() => {
+    const client = socket("http://localhost:5000");
+    client.on("chat", message => {
+      setChat(prevChats => [...prevChats, message])
+    });
+    socketClientRef.current = client
+    return () => {
+      client.removeAllListeners()
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Chatty App</h1>
+
+        {chat && chat.map((payload, index) => {
+          return (
+            <>
+              <div className={`${userName === payload.userName ? 'sender' : 'receiver'}`}>
+                <p key={index}>{payload.message}</p>
+                {userName !== payload.userName && (
+                  <span>{payload.userName}</span>
+                )}
+              </div>
+            </>
+          )
+        })}
+
+        <form onSubmit={sendChat}>
+          <input
+            type="text"
+            name="chat"
+            placeholder="send text"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+
+          <button disabled={!message} type="submit">Send</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
